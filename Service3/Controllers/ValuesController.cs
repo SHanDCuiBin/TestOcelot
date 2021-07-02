8e2a104da7f15fbdc3ce10da1998bb8387267e43
cuibin <sdcuibin@outlook.com>
2021-07-02 22:27:40 +0800

    添加项目文件。

---
+++ Service3/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Service3.Controllers
+{
+    /// <summary>
+    /// 测试 Control
+    /// </summary>
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        /// <summary>
+        /// 获取Service1 检查结果
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public string Get()
+        {
+            return "This is Service3 Get!";
+        }
+    }
+}
